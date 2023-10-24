3928796fdb2e901f6a460aa94af9d614dcccaaa4
fadesyphe <pivkin.jura2017@gmail.com>
2023-10-24 14:41:51 +0600

    added contact, logic testimonial sliders(fixed logic!)

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+	anchor.addEventListener('click', (e) => {
+		e.preventDefault();
+		const blockID = anchor.getAttribute('href');
 
+		document.querySelector(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}
+
+
+
+
+
+const testimonialCarousel = document.querySelector('.testimonial_slider_carousel');
+const testimonialItems = Array.from(document.querySelectorAll('.testimonial_info'));
+const dots = Array.from(document.querySelectorAll('.dot'));
+for (let i = 0; i < dots.length; i++) {
+	dots[i].addEventListener('click', (e) => {
+		let dotActive = document.querySelector('.dot.active');
+		let testimonialItemActive = document.querySelector('.testimonial_info--active');
+
+		if (dotActive === dots[i]) {
+			return
+		}
+
+		let testimonialOffset = 605 * (-i); //???? fix logic offset
+		testimonialCarousel.style.transition = '0.5s ease';
+		testimonialCarousel.style.left = `${testimonialOffset}px`;
+		
+		dotActive.classList.remove('active');
+		dots[i].classList.add('active');
+
+		testimonialItemActive.classList.remove('testimonial_info--active');
+		testimonialItems[i].classList.add('testimonial_info--active');
+	})
+}
